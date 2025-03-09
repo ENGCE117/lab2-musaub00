44ecea9099289432b869f32766a67ea362888f96
musaub <musaup2424@gmail.com>
2025-03-09 19:37:01 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[] ) ;
 
 int main() {
-    //YOUR CODE HERE
+
+    char text[ 50 ] ;
+    char *output ;
+
+    printf( "Enter str1 : " ) ;
+    fgets( text , sizeof( text ) , stdin ) ;
+
+    text[ strcspn( text , "\n" ) ] = '\0' ;
+
+    output = reverse( text ) ;
+
+    printf( "MAIN : %s\n" , output ) ;
+
     return 0 ;
-}//end function
+}
+
+char* reverse( char str1[] ) {
+
+    int len = strlen( str1 ) ;
+    char temp ;
+
+    for ( int i = 0 ; i < len / 2 ; i++ ) {
+        temp = str1[ i ] ;
+        str1[ i ] = str1[ len - i - 1 ] ;
+        str1[ len - i - 1 ] = temp ;
+    }
+
+    printf( "FUNC : %s\n" , str1 ) ;
+
+    return str1 ;
+}
